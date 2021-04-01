320c9b30adcd73fc2aec3db1170ce31ef0dedbe9
tototomoya <kt1206hp@gmail.com>
2021-04-01 04:52:27 +0000

    お金フォルダの大きな変更

---
+++ 自販機/Code/お金/Money.java
@@ -0,0 +1,58 @@
+package 自販機;
+
+abstract class Money {
+    protected int type;
+    protected int num = 0;
+
+    public int get_num() {
+        return num;
+    }
+
+    public int sum() {
+        return type * num;
+    }
+
+    public void add(int num) {
+        this.num += num;
+    }
+
+    public void add() {
+        num++;
+    }
+
+    public void subtract() {
+        num--;
+    }
+
+    public void subtract(int num) {
+        this.num -= num;
+    }
+}
+
+class Hyaku extends Money {
+    public Hyaku(){
+        super.type = 100;
+    }
+}
+
+class Sen extends Money {
+    public Sen(){
+        super.type = 1000;
+    }
+}
+
+class Ju extends Money {
+    public Ju(){
+        super.type = 10;
+    }
+}
+class Go_ju extends Money {
+    public Go_ju(){
+        super.type = 50;
+    }
+}
+class Go_hyaku extends Money {
+    public Go_hyaku(){
+        super.type = 500;
+    }
+}
