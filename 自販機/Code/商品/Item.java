56c7eeec1a2a636201c0abb1b1a5715a4a12122f
Tomoya <57219242+tototomoya@users.noreply.github.com>
2021-04-07 00:00:07 +0900

    Update Item.java

--- 自販機/Code/商品/Item.java
+++ 自販機/Code/商品/Item.java
@@ -2,17 +2,17 @@ package 自販機;
 
 class Item {
 
-    public String name;
-    public int value;
-    public int num;
-    public boolean hot;
+	public String name;
+	public int value;
+	public int num;
+	public boolean hot;
     
-    public Item(String name, int value, int num, boolean hot) {
-        this.name = name;
-        this.value = value;
-        this.num = num;
-        this.hot = hot;
-    }
+	public Item(String name, int value, int num, boolean hot) {
+		this.name = name;
+		this.value = value;
+		this.num = num;
+		this.hot = hot;
+	}
 
 	public void drawItemNum() {
 		this.num = this.num - 1;
