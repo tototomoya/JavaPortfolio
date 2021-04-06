776d3ba226bbd103865d0c8e5d7793a29dd5fe28
Tomoya <57219242+tototomoya@users.noreply.github.com>
2021-04-06 23:57:58 +0900

    Update B.java

--- 自販機/Code/Location/B.java
+++ 自販機/Code/Location/B.java
@@ -2,18 +2,18 @@ package 自販機;
 
 class B extends Location {
 	
-	/*
-	
-	single tone(thread safe)
+/*
 
-	webアプリのようなステートレスなプログラムではないので
-	使用しました
-	
-	*/
+single tone(thread safe)
+
+webアプリのようなステートレスなプログラムではないので
+使用しました
+
+*/
     private static B instance = new B();
     private void B(){}
 	static {
-		// 自販機の商品データ読み込み
+// 自販機の商品データ読み込み
 		instance.readItemsList("B_itemsList.csv");
 		instance.locationName = "B";
 	}
